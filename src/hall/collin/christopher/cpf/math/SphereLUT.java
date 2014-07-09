d47e31f977a0dd54a428433ca25dc571a28c9082
CCHall <>
2014-07-09 09:19:35 -0400

    Added GPL header

--- src/hall/collin/christopher/cpf/math/SphereLUT.java
+++ src/hall/collin/christopher/cpf/math/SphereLUT.java
@@ -1,3 +1,27 @@
+/*
+Cyano's Planet Factory
+
+Copyright (C) 2014 Christopher Collin Hall
+email: explosivegnome@yahoo.com
+
+Cyano's Planet Factory is distributed under the GNU General Public 
+License (GPL) version 3.
+
+Cyano's Planet Factory is free software: you can redistribute it 
+and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of 
+the License, or (at your option) any later version.
+
+Cyano's Planet Factory is distributed in the hope that it will be 
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Cyano's Planet Factory.  If not, see 
+<http://www.gnu.org/licenses/>.
+
+*/
 
 package hall.collin.christopher.cpf.math;
 
