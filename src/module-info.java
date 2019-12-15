250f5dbcc373a967423c8fe6250e5a1e523a440b
Christopher Hall <cchall@localhost.localdomain>
2019-12-16 08:49:38 +1100

    It works in JDK 13!

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module cchall.cyanosplanetfactory {
+	requires java.desktop;
+	requires java.logging;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.base;
+	requires name.cchall.globeviewer;
+	requires nanojson;
+	requires CCH.World.Factory.GPL;
+}
